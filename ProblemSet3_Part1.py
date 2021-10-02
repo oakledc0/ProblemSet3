32786d99dfe22cbc3b23982074b1a14c8374f61f
Cal Oakley <daniel.oakley@duke.edu>
2021-10-02 09:39:56 -0400

    Setting up workspace for Problem Set 3

---
+++ ProblemSet3_Part1.py
@@ -0,0 +1,15 @@
+#-----------------------------------------------------------------------------
+#
+# Author: Cal Oakley
+# Date: Fall 2021
+#
+#-----------------------------------------------------------------------------
+# %% /*-PS3: Code Block Task 1--*/
+
+mountain = "Denali"
+nickname = 'Mt. McKinley'
+elevation = "20322" 
+
+print(mountain + ', formerly\nknown as "' + nickname + '",')
+print("is " + elevation + ' above sea level.' )
+# %% /*-PS3: Code Block Task 2--*/
